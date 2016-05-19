1ef0100dbbd05c6727eb602bea49eaeae6a4fdee
Laurent Peuch <cortex@worlddomination.be>
2016-05-19 02:21:41 +0200

    [mod] add somedebug output

--- ci.py
+++ ci.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import vagrant
 from urllib import urlretrieve
 
@@ -6,9 +7,12 @@ VAGRANTFILE_VERSION = "834e48942903e7c1069bbdae278888a078201bc3"
 
 
 def main():
-    if not os.path.exists("vagrantfile") or not os.path.exists(VAGRANTFILE_VERSION):
+    if not os.path.exists("Vagrantfile") or not os.path.exists(VAGRANTFILE_VERSION):
+        sys.stdout.write("Retreiving Vagrantfile ...")
+        sys.stdout.flush()
         urlretrieve("https://raw.githubusercontent.com/YunoHost/Vagrantfile/%s/Vagrantfile" % VAGRANTFILE_VERSION, "Vagrantfile")
         open(VAGRANTFILE_VERSION, "w").write("")
+        sys.stdout.write(" done\n")
 
     v = vagrant.Vagrant()
 
